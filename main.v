ff54de2a29effc5819f6786e60b78e918db25e79
Pedro Cruz <60475001+pedroc07@users.noreply.github.com>
2024-07-17 17:14:08 -0300

    Update main.v

--- main.v
+++ main.v
@@ -1,16 +1,20 @@
 // Declaracao do modulo
-module main (Clk, Rst, Us, Nv1, Nv0, Bs, Vs, A, G);  
+module main (Clk, Rst, Us, Nv1, Nv0, Bs, Vs, A, G, Adub, Ve, Mist, Limp0, Limp1);  
 
 	// Declaracao de portas
-	input Clk, Rst, Bs, Us, Vs, Nv1, Nv0;
-	output A, G;
+	input Clk, Rst, Bs, Us, Vs, Nv1, Nv0, Adub;
+	output A, G, Ve, Mist, Limp0, Limp1;
 	
 	// Declaracao dos fios intermediarios
-	wire Wire_us, REGA;
+	wire Asp, REGA, wire_Adub;
+	assign Asp = A;
+	assign Limp1 = Limp0;
+	not not0(wire_Adub, Adub);
 	
 	// Funcionamento do circuito
 	
 	Irrigacao(Us, Nv0, Bs, Vs, REGA);
 	mef_tiporega ( Clk, Rst, REGA, Vs, Bs, A, G) ;
+	mef_adub_limp (Clk, Rst, wire_Adub, Nv1, Nv0, Asp, Ve, Mist, Limp0);
 	
 endmodule
