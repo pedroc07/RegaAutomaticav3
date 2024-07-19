d702a1b4d7c632c74d39c1e729c4313e2c0f30a7
Pedro Cruz <60475001+pedroc07@users.noreply.github.com>
2024-07-19 09:43:15 -0300

    Update mef_adub_limp.v

--- mef_adub_limp.v
+++ mef_adub_limp.v
@@ -6,7 +6,7 @@ module mef_adub_limp ( input clk ,
 	parameter B = 2'b01 ;
 	parameter C = 2'b10 ;
 	parameter D = 2'b11 ;
-	wire wire0, wire1, wire2, wire3, cond0, cond1, cond2, cond3, notA, notAdub, notNv1, notNv0;
+	wire wire0, wire1, wire2, wire3, wire4, wire5, cond0, cond1, cond2, cond3, cond4, cond5, notA, notAdub, notNv1, notNv0;
 	not not0(notA, Asp);
 	not not1(notAdub, Adub);
 	not not2(notNv1, Nv1);
@@ -18,7 +18,11 @@ module mef_adub_limp ( input clk ,
 	or or0(cond0, wire0, wire1);
 	or or1(cond1, wire2, wire3);
 	and and4(cond2, Nv1, Nv0);
-	and or2(cond3, notNv1, notNv0);
+	and and5(wire4, Nv1, notA);
+	and and6(wire5, Nv0, notA);
+	or or2(cond3, wire4, wire5);
+	or or3(cond4, notNv1, notNv0);
+	and and7(cond5, notNv0, notNv1, notA);
 	// state register
 	always @ ( posedge clk, posedge reset)
     	if ( reset ) state <= A ;
@@ -26,8 +30,9 @@ module mef_adub_limp ( input clk ,
     	// next state logic
 	always @ (*)begin
         	if(state == A) begin
-            	if (notA) nextstate <= A ;
-            	else nextstate <= B ;
+            	if (cond3) nextstate <= A ;
+            	else if(cond5) nextstate <= D ;
+					else nextstate <= B ;
         	end
         	else if(state == B) begin
             	if (notA) nextstate <= A ;
@@ -45,7 +50,7 @@ module mef_adub_limp ( input clk ,
         	else nextstate <= A;
         	end
 	// output logic
-	assign Ve = (cond3 & (state == D)) ;
+	assign Ve = (cond4 & (state == D)) ;
 	assign Mist = (Nv1 & (state == C));
 	assign Limp = (notNv1 & (state == C)) ;
     
